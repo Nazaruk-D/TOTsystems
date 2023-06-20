8cfbccaed1f6f27f442ebf644085241dd893b08b
Nazaruk-D <nazaruk-dima@mail.ru>
2023-06-20 18:00:52 +0300

    fix: redirect to homepage when user is logged in

--- src/app/Root/Root.tsx
+++ src/app/Root/Root.tsx
@@ -4,13 +4,20 @@ import { Box } from '@mui/material';
 import s from './Root.module.scss';
 import MessageSnackbar from '../../common/components/MessageSnackbar/MessageSnackbar';
 import Header from '../Header/Header';
+import { useAppSelector } from '../../hooks/useRedux';
+import { isLoggedInSelector } from '../../store/selectors/userSelector';
+import { Path } from '../../enums/path';
 
 const Root = () => {
     const navigate = useNavigate();
+    const isLoggedIn = useAppSelector(isLoggedInSelector);
 
     useEffect(() => {
-        navigate('/messages');
+        if (isLoggedIn) {
+            navigate(Path.Messages);
+        }
     }, []);
+
     return (
         <Box className={s.rootContainer}>
             <Header />
